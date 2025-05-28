d82816632e3173e389bc2bbe03072d07e7417ed1
Riyansh Prakash <riyanshprakash007@gmail.com>
2025-05-28 16:37:56 +0530

    fix src file

--- server/utils/db.js
+++ server/utils/db.js
@@ -7,7 +7,7 @@ const { database } = keys;
 
 const setupDB = async () => {
   try {
-    // Connect to MongoDB
+    // Connect to MongoDB database.url
     mongoose.set('useCreateIndex', true);
     mongoose
       .connect(database.url, {
